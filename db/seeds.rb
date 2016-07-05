879e48c0a6cfd7f71f4bb25cde5d156cd6787938
Raul Galindo <raul.galindo@xing.com>
2016-07-05 12:24:14 +0200

    add seeds to my app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create(name:"Crazy project", description:"you wont believe it")
+Project.create(name:"Time tracking app")
+Project.create(name:"Recipees", description:"for all the foodies")
+Project.create(name:"Crazy project2", description:"2you wont believe it")
+Project.create(name:"Time tracking app3")
+Project.create(name:"Recipees3", description:"for all the foodie345s")
+Project.create(name:"Crazy project5", description:"you wont beli34534eve it")
+Project.create(name:"Time tracking5 app")
+Project.create(name:"Recipee234s", description:"for all th345435e foodies")
+Project.create(name:"Crazy proj456ect", description:"you43 wont believe it")
