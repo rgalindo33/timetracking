4d8ba5c47279bbb8ae673a9bb1aea437f559ba00
Raul Galindo <raul.galindo@xing.com>
2016-07-06 09:38:00 +0200

    add show route to our projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,12 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.order(created_at: :desc).limit(10)
   end
+
+  def show
+    @my_project = Project.find_by(id: params[:id])
+
+    unless @my_project
+      render "no_projects_found"
+    end
+  end
 end
